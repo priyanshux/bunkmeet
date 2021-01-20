b73f4d184d1110c60bfe45e5206b49b6b513d13b
Priyanshu <priyanshux@iitkgp.ac.in>
2021-01-20 22:23:10 +0530

    new branch

--- server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-
-export FLASK_APP=run.py 
-
-flask run --host=localhost
--- worker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source venv/bin/activate
-
-celery -A bunkmeet.routes.celery worker --loglevel=info
