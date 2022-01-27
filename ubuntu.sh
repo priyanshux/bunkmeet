d154dff87749d6d4f103fd01cc1d1b02efb04016
Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
2022-01-27 14:48:45 +0530

    Add method to fetch chromedriver

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,7 +4,20 @@ sudo apt-get -y update
 
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4 google-chrome-stable
 
-wget http://chromedriver.storage.googleapis.com/98.0.4758.48/chromedriver_linux64.zip
+#Check the latest chrome driver available for the installed google-chrome-stable
+#For more information, see : https://chromedriver.chromium.org/downloads/version-selection
+chrome_version=$(/usr/bin/google-chrome-stable --version | grep -Eoh '[0-9,.]+')
+echo "Chrome Version : $chrome_version "
+
+a=( ${chrome_version//./ } )                   # replace points, split into array
+chrome_version="${a[0]}.${a[1]}.${a[2]}"       # compose new version
+
+latest_release_url="https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$chrome_version"
+latest_release=$(curl $latest_release_url)
+echo "Chrome Driver Version : $latest_release"
+
+# Fetch the latest chromedriver
+wget http://chromedriver.storage.googleapis.com/$latest_release/chromedriver_linux64.zip
 
 unzip chromedriver_linux64.zip
 
